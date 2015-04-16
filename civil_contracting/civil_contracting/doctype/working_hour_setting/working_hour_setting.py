b8271a7054a42c77924f4a12fcbd996db4516487
Revant Nandgaonkar <revant.one@gmail.com>
2015-04-16 12:40:04 +0530

    Deleted Working hours settings, added Worker Sheet Settings

--- civil_contracting/civil_contracting/doctype/working_hour_setting/working_hour_setting.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2013, Revant Nandgaonkar and contributors
-# For license information, please see license.txt
-
-from __future__ import unicode_literals
-import frappe
-from frappe.model.document import Document
-
-class WorkingHourSetting(Document):
-	pass
