d3d983d85d525a022c18968f321d7342c5ee0ce8
Revant Nandgaonkar <revant.one@gmail.com>
2015-04-19 11:47:44 +0530

    Removed Default payment account filter

--- civil_contracting/civil_contracting/doctype/worker_sheet_settings/worker_sheet_settings.js
+++ civil_contracting/civil_contracting/doctype/worker_sheet_settings/worker_sheet_settings.js
@@ -2,13 +2,7 @@
 
 
 /* Set Account Selection filter to show only Ledgers with group_or_ledger is "Ledger" */
-cur_frm.set_query("payment_account", function(doc) {
-	return {
-		"filters": {
-			"group_or_ledger": "Ledger"
-		}
-	};
-});
+
 cur_frm.set_query("wages_account", function(doc) {
 	return {
 		"filters": {
