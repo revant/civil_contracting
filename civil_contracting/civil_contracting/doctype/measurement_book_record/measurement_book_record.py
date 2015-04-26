458f7a2919864150e077600048cb8900e03e0b96
Revant Nandgaonkar <revant.one@gmail.com>
2015-04-26 16:18:18 +0530

    Updated Measurements (Sheet and Book)

--- civil_contracting/civil_contracting/doctype/measurement_book_record/measurement_book_record.py
+++ civil_contracting/civil_contracting/doctype/measurement_book_record/measurement_book_record.py
@@ -4,11 +4,6 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from erpnext.controllers.print_settings import print_settings_for_item_table
 
 class MeasurementBookRecord(Document):
-	def __setup__(self):
-		print_settings_for_item_table(self)
-		
-	def calculate_item_values(self):
-		item.qty = flt(item.length * item.breadth * (item.depth or 1), self.precision("amount", item))
+	pass
