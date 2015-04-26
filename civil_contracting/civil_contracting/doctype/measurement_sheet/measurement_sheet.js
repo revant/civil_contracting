4f63a91900ad2758cb7f28939b27c5985094f850
Revant Nandgaonkar <revant.one@gmail.com>
2015-04-26 16:30:52 +0530

    Cleaned files

--- civil_contracting/civil_contracting/doctype/measurement_sheet/measurement_sheet.js
+++ civil_contracting/civil_contracting/doctype/measurement_sheet/measurement_sheet.js
@@ -1,15 +1,5 @@
 // See license.txt
 
-
-/* Set BOM selection filter to show only Draft BOM, BOM with docstatus 0 */
-cur_frm.set_query("bom_no", function(doc) {
-	return {
-		"filters": {
-			"docstatus": 0
-		}
-	};
-});
-
 calculate_total_qty = function(doc) {
 	var measurements = doc.measurement_table || [];
 	doc.total_qty = 0.0;
@@ -24,12 +14,4 @@ cur_frm.cscript.validate = function(doc, dt, dn) {
 	calculate_total_qty(doc);
 }
 
-/*
-// TODO: Add comment "Previous Quantity: Quantity in BOM before updating, update and Current Quantity
-
-// TODO: Set total_qty as quantity of selected BOM on Submit
-cur_frm.cscript.on_submit = function(doc, dt, dn) {
-	bom_no = doc.bom_no;
-	frappe.model.set_value('BOM', bom_no.name, "quantity", total_qty);
-}
-*/
+cur_frm.add_fetch("item", "stock_uom", "uom");
