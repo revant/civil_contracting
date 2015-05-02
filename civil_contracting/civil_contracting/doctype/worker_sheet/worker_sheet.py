96f87e2360b91cc577563708eba46f7e1f859e1f
Revant Nandgaonkar <revant.one@gmail.com>
2015-05-02 21:48:06 +0530

    Duplicate Employee in a Worker Sheet Attendance Not allowed

--- civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
+++ civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
@@ -4,10 +4,21 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import msgprint
+from frappe import _
 from frappe.model.document import Document
 from frappe.database import *
 
 class WorkerSheet(Document):
 	def validate(self):
-		# What to validate?
-		pass
+		# validate repeating employees in attendance
+		self.validate_for_employee()
+
+	def validate_for_employee(self):
+		check_list = []
+		for d in self.get('worker_attendance'):
+			if d.worker_name:
+				check_list.append(cstr(d.worker_name))
+		
+		unique_chk_list = set(check_list)
+		if len(unique_chk_list) != len(check_list):
+			frappe.throw(_("Warning: Same Employee has been entered multiple times."))
