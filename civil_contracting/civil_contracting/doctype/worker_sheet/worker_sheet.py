9d1cb8bcf5542ee700c1a69d6c4d3297f9beba42
Revant Nandgaonkar <revant@revant.me>
2015-05-30 10:45:49 +0530

    Removed non-working code - validating employees

--- civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
+++ civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
@@ -9,16 +9,5 @@ from frappe.model.document import Document
 from frappe.database import *
 
 class WorkerSheet(Document):
-	def validate(self):
-		# validate repeating employees in attendance
-		self.validate_for_employee()
+	pass
 
-	def validate_for_employee(self):
-		check_list = []
-		for d in self.get('worker_attendance'):
-			if d.worker_name:
-				check_list.append(cstr(d.worker_name))
-		
-		unique_chk_list = set(check_list)
-		if len(unique_chk_list) != len(check_list):
-			frappe.throw(_("Warning: Same Employee has been entered multiple times."))
