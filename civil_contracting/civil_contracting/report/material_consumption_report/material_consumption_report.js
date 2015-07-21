f5b669bd71dddae282ed3eaedb2c79f06896613a
Revant Nandgaonkar <revant.one@gmail.com>
2015-07-21 14:04:26 +0530

    Added Material Sheet and Report stub

---
+++ civil_contracting/civil_contracting/report/material_consumption_report/material_consumption_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2013, Revant Nandgaonkar and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Material Consumption Report"] = {
+	"filters": [
+
+	]
+}
