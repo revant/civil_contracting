f58a770b53127cb3a12e9593f269fa81ece0750f
Revant Nandgaonkar <revant.one@gmail.com>
2015-07-29 12:43:23 +0530

    Added Output Qty Column in Material Consumption Report

--- civil_contracting/civil_contracting/report/material_consumption_report/material_consumption_report.py
+++ civil_contracting/civil_contracting/report/material_consumption_report/material_consumption_report.py
@@ -13,16 +13,16 @@ def execute(filters=None):
 	for mat_ent in material_entries:
 		mat_item = get_mat_item(mat_ent.name)
 		for i in mat_item:
-			data.append([mat_ent.date, mat_ent.name, i.item_consumed, i.item_description, i.qty_consumed, i.item_uom])
+			data.append([mat_ent.date, mat_ent.name, mat_ent.op_qty, i.item_consumed, i.item_description, i.qty_consumed, i.item_uom])
 		#data.append([mat_ent.date, mat_ent.name, mat_ent.project, mat_ent.output_item])
 	return columns, data
 
 def get_columns():
-	return [_("Date") + ":Date:95", _("Sheet No.") + ":Link/Material Sheet:95", _("Item") + ":Data:95", _("Description") + ":Data:145", _("Qty Consumed") + ":Data:95", _("UOM") + ":Data:95"
+	return [_("Date") + ":Date:95", _("Sheet No.") + ":Link/Material Sheet:95", _("O/P Qty") + ":Float:", _("Item") + ":Data:95", _("Description") + ":Data:145", _("Qty Consumed") + ":Data:95", _("UOM") + ":Data:95"
 	]
 
 def get_material_entries(filters):
-	return frappe.db.sql("""select date, name, project, output_item
+	return frappe.db.sql("""select date, name, project, output_item, op_qty
 		from `tabMaterial Sheet`
 		where output_item = %(output_item)s and
 		project = %(project)s and
