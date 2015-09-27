3333239fb646a89a0cd4c57c07bf7836ed21b0d2
Revant Nandgaonkar <revant.one@gmail.com>
2015-09-27 14:25:24 +0530

    [Fix] Get Items only works when DN is selected

--- civil_contracting/civil_contracting/doctype/measurement_book/measurement_book.js
+++ civil_contracting/civil_contracting/doctype/measurement_book/measurement_book.js
@@ -17,27 +17,28 @@ frappe.ui.form.on("Measurement Book", "get_items", function(frm) {
 	// erpnext.payment_tool.check_mandatory_to_fetch(frm.doc);
 
 	frm.set_value("records", []);
+	if(frm.doc.delivery_note) {
+		return  frappe.call({
+			method: 'civil_contracting.civil_contracting.doctype.measurement_book.measurement_book.get_measure_sheets',
+			args: {
+				"dn": frm.doc.delivery_note
+			},
+			callback: function(r, rt) {
+				if(r.message) {
+					
+					frm.clear_table("records");
 
-	return  frappe.call({
-		method: 'civil_contracting.civil_contracting.doctype.measurement_book.measurement_book.get_measure_sheets',
-		args: {
-			"dn": frm.doc.delivery_note
-		},
-		callback: function(r, rt) {
-			if(r.message) {
-				
-				frm.clear_table("records");
-
-				$.each(r.message, function(i, d) {
-					var c = frm.add_child("records");
-					c.sheet_no = d.name;
-					c.item_name = d.item;
-					c.total_qty = d.total_qty;
-					c.uom = d.uom;
-				});
+					$.each(r.message, function(i, d) {
+						var c = frm.add_child("records");
+						c.sheet_no = d.name;
+						c.item_name = d.item;
+						c.total_qty = d.total_qty;
+						c.uom = d.uom;
+					});
+				}
+				refresh_field("records");
+				frm.layout.refresh_sections();
 			}
-			refresh_field("records");
-			frm.layout.refresh_sections();
-		}
-	});
+		});
+	}
 });
