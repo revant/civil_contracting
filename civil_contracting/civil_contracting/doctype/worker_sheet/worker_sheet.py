c3a797ca2b475e8471dae35ec9f61d6b741ab2bb
Revant Nandgaonkar <revant.one@gmail.com>
2015-09-30 12:46:39 +0530

    Worker, Wage Slip added for OS Wage management

--- civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
+++ civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
@@ -9,5 +9,14 @@ from frappe.model.document import Document
 from frappe.database import *
 
 class WorkerSheet(Document):
-	pass
-
+	def on_submit(self):
+		for worker in self.worker_attendance:
+			if worker.worker_name:
+				os_wages = worker.rate * worker.hours
+				frappe.set_value("Worker", worker.worker_name, "outstanding_wages", os_wages)
+	def on_cancel(self):
+		for worker in self.worker_attendance:
+			if worker.worker_name:
+				old_os_wages = frappe.db.get_value("Worker", {"name":worker.worker_name}, "outstanding_wages")
+				os_wages = old_os_wages - (worker.rate * worker.hours)
+				frappe.set_value("Worker", worker.worker_name, "outstanding_wages", os_wages)
