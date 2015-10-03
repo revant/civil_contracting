161c4153eaac2f89957c80bacf59622f6559c0ab
Revant Nandgaonkar <revant.one@gmail.com>
2015-10-03 16:37:08 +0530

    fix in Worker Sheet > Worker OS Wages

--- civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
+++ civil_contracting/civil_contracting/doctype/worker_sheet/worker_sheet.py
@@ -12,7 +12,8 @@ class WorkerSheet(Document):
 	def on_submit(self):
 		for worker in self.worker_attendance:
 			if worker.worker_name:
-				os_wages = worker.rate * worker.hours
+				old_os_wages = frappe.db.get_value("Worker", {"name":worker.worker_name}, "outstanding_wages")
+				os_wages = old_os_wages + (worker.rate * worker.hours)
 				frappe.set_value("Worker", worker.worker_name, "outstanding_wages", os_wages)
 	def on_cancel(self):
 		for worker in self.worker_attendance:
