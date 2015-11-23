dcc720d8172f8e2b00642d51ca245bb0f869e0b1
Revant Nandgaonkar <revant.one@gmail.com>
2015-11-23 14:38:08 +0530

    Pre-release Running Account Bill Doctype

--- civil_contracting/civil_contracting/doctype/running_account_bill/running_account_bill.js
+++ civil_contracting/civil_contracting/doctype/running_account_bill/running_account_bill.js
@@ -21,13 +21,18 @@ frappe.ui.form.on("Running Account Bill", "prev_ra_bill", function(frm) {
 });
 
 frappe.ui.form.on("Running Account Bill", "get_invoices", function(frm) {
+	var sum_net_total = 0.0;
+	var sum_tax = 0.0;
+	var sum_grand_total = 0.0;
+	var sum_paid_amount = 0.0;
 	frm.set_value("sales_invoice_list", []);
 	if(frm.doc.customer) {
 		return frappe.call({
 			method: 'civil_contracting.civil_contracting.doctype.running_account_bill.running_account_bill.get_sales_invoices',
 			args: {
 				"customer": frm.doc.customer,
-				"project": frm.doc.project
+				"project": frm.doc.project,
+				"prev_ra_bill": frm.doc.prev_ra_bill
 			},
 			callback: function(r, rt) {
 				if(r.message) {
@@ -38,13 +43,48 @@ frappe.ui.form.on("Running Account Bill", "get_invoices", function(frm) {
 						var c = frm.add_child("sales_invoice_list");
 						c.sales_invoice = d.name;
 						c.net_total = d.total;
+						sum_net_total += d.total; // build sum_net_total
 						c.tax = d.total_taxes_and_charges;
+						sum_tax += d.total_taxes_and_charges; // build sum_tax
 						c.grand_total = d.grand_total;
+						sum_grand_total += d.grand_total; // build sum_grand_total
+						sum_paid_amount += d.paid_amount; // build sum_paid_amount
 					});
 				}
+				frm.set_value("sum_net_total", sum_net_total);
+				frm.set_value("sum_taxes", sum_tax);
+				frm.set_value("sum_grand_total", sum_grand_total);
+				frm.set_value("payments_till_date", sum_paid_amount);
 				refresh_field("sales_invoice_list");
 				frm.layout.refresh_sections();
 			}
 		});
 	}
 });
+
+
+frappe.ui.form.on("Running Account Bill", "get_items", function(frm) {
+	frm.set_value("items_list", []);
+	return frappe.call({
+		method: 'get_ra_items',
+		doc: frm.doc,
+		callback: function(r, rt) {
+			if(r.message) {
+				frm.clear_table("items_list");
+				$.each(r.message, function(i, d) {
+					var c = frm.add_child("items_list");
+					c.item = d.item_code;
+					c.current_qty = d.qty;
+					c.uom = d.stock_uom;
+					c.previous_qty = d.previous_qty;
+					c.total_qty = flt(c.current_qty) + flt(c.previous_qty);
+					c.rate = d.rate;
+					c.amount = c.total_qty * c.rate;
+				});
+			}
+			refresh_field("items_list");
+			frm.layout.refresh_sections();
+		}
+	});
+});
+
--- civil_contracting/civil_contracting/doctype/running_account_bill/running_account_bill.py
+++ civil_contracting/civil_contracting/doctype/running_account_bill/running_account_bill.py
@@ -4,43 +4,113 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe import _
+from frappe import _, msgprint
 from frappe.model.document import Document
+from collections import Counter
 
 class RunningAccountBill(Document):
+
 	def validate(self):
 		self.validate_previous_ra_bill()
 		self.validate_project()
 		self.set_ra_bill_no()
+		self.validate_ra_no()
+		self.validate_prev_si()
+
+	def validate_prev_si(self):
+		if self.prev_ra_bill:
+			for invoice in self.sales_invoice_list:
+				prev_ra_bill = frappe.get_list("Running Account Bill Invoices",
+					fields=["sales_invoice"],
+					filters = {
+						"parent": self.prev_ra_bill
+					})
+				for i in xrange(len(prev_ra_bill)):
+					if invoice.sales_invoice == prev_ra_bill[i].sales_invoice:
+						frappe.throw(_("Cannot Select Sales Invoices from Previous RA Bill"))
+
+	def validate_ra_no(self):
+		""" Validates Same RA Bill cannot be selected as Previous RA Bill"""
+		if self.prev_ra_bill and self.prev_ra_bill == self.name:
+			frappe.throw(_("Cannot Select Previous RA Bill on Same Document"))
 
 	def validate_previous_ra_bill(self):
+		"""Validate customer of current RA Bill and Previous RA Bill to be equal"""
 		if self.prev_ra_bill and frappe.db.get_value("Running Account Bill", self.prev_ra_bill, "customer") != self.customer:
 			frappe.throw(_("Previous Running Account Bill Customer does not match with current"))
 
 	def validate_project(self):
+		"""Validate Project's Customer and RA Bill's Customer to be same"""
 		if self.project and frappe.db.get_value("Project", self.project, "customer") != self.customer:
 			frappe.throw(_("Project's Customer does not match with current"))
 
 	def set_ra_bill_no(self):
+		"""Set RA Bill No depending on Selected Previous RA Bill or set it to 1"""
 		if self.prev_ra_bill:
-			prev_ra_bill_no = frappe.db.get_value("Running Account Bill", self.prev_ra_bill, "ra_bill_no") or 0
+			prev_ra_bill_no = frappe.db.get_value("Running Account Bill", self.prev_ra_bill, "ra_bill_no")
 			self.ra_bill_no = int(prev_ra_bill_no) + 1
 		else:
 			self.ra_bill_no = 1
 
+	def get_ra_items(self):
+		"""get_ra_items"""
+		item_list = []
+		out_list = []
+		for i in self.get("sales_invoice_list"):
+			item_list.append(frappe.db.sql("""select item_code, qty, rate, stock_uom, amount
+			from `tabSales Invoice Item`
+			where parent = %(parent)s"""\
+			, {"parent":i.sales_invoice}, as_dict=1))
+
+		for item in item_list:
+			for i in range(0,len(item)):
+				out_list.append({
+					"item_code": item[i].item_code,
+					"qty": item[i].qty,
+					"stock_uom": item[i].stock_uom,
+					"rate": item[i].rate,
+					"amount": item[i].amount,
+					"previous_qty": 0.0
+				})
+
+		for i,a in enumerate(out_list):
+		    z=0
+		    for b in out_list[i+1:]:
+		        if a['item_code']==b['item_code']:
+		            a['qty'] += b['qty']
+		            out_list.pop(out_list.index(b))
+		            z=1
+		
+		if self.prev_ra_bill:
+			for item in out_list:
+				item['previous_qty'] = frappe.db.get_value("Running Account Bill Items", {"parent":self.prev_ra_bill, "item":item['item_code']}, "total_qty")
+
+		return out_list
+
 @frappe.whitelist()
-def get_sales_invoices(customer, project):
+def get_sales_invoices(customer, project=None, prev_ra_bill=None):
 	if customer and project:
-		return frappe.db.sql("""select name, total, total_taxes_and_charges, grand_total
+		out = frappe.db.sql("""select name, total, total_taxes_and_charges, grand_total, paid_amount
 			from `tabSales Invoice`
 			where customer = %(customer)s and
 			project_name = %(project)s and
 			docstatus = 1 """\
-			, {"customer":frappe.db.escape(customer),"project":frappe.db.escape(project) or "%"}, as_dict=1)
-
-	if customer:
-			return frappe.db.sql("""select name, total, total_taxes_and_charges, grand_total
+			, {"customer":frappe.db.escape(customer),"project":frappe.db.escape(project)}, as_dict=1)
+	elif customer:
+		out = frappe.db.sql("""select name, total, total_taxes_and_charges, grand_total, paid_amount
 			from `tabSales Invoice`
 			where customer = %(customer)s and
 			docstatus = 1 """\
-			, {"customer":frappe.db.escape(customer),"project":frappe.db.escape(project) or "%"}, as_dict=1)
+			, {"customer":frappe.db.escape(customer)}, as_dict=1)
+
+	ra = frappe.get_list("Running Account Bill Invoices",
+			fields=["sales_invoice"],
+			filters = {
+				"parent": prev_ra_bill
+			})
+	for i in xrange(len(ra)):
+		for item in out:
+			if ra[i].sales_invoice == item.name:
+				out.remove(item)
+
+	return out
